b32f5c70f6c16980ab5c4dfecdd4550eed702003
tuanthanh2067 <tuanthanh2067@gmail.com>
2021-02-25 17:59:58 -0500

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,9 +19,11 @@ import { MatChipsModule } from '@angular/material/chips';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { NewReleasesComponent } from './new-releases/new-releases.component';
+import { AlbumComponent } from './album/album.component';
 
 @NgModule({
-  declarations: [AppComponent, AboutComponent, NotFoundComponent],
+  declarations: [AppComponent, AboutComponent, NotFoundComponent, NewReleasesComponent, AlbumComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/new-releases/new-releases.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+import * as data from '../data/NewReleasesAlbums.json';
+
+@Component({
+  selector: 'app-new-releases',
+  templateUrl: './new-releases.component.html',
+  styleUrls: ['./new-releases.component.css'],
+})
+export class NewReleasesComponent implements OnInit {
+  releases: Array<any>;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.releases = data.albums.items;
+    console.log(this.releases);
+  }
+}
