1ef89f8dbf22bc0013c0b06729be67c27f7c4e82
tuanthanh2067 <tuanthanh2067@gmail.com>
2021-02-26 11:42:02 -0500

    update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
-// import { AboutComponent } from './about/about.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AboutComponent } from './about/about.component';
+import { AlbumComponent } from './album/album.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { NewReleasesComponent } from './new-releases/new-releases.component';
+import { ArtistDiscographyComponent } from './artist-discography/artist-discography.component';
+
 const routes: Routes = [
-  // { path: 'newRelease' },
-  // { path: 'about', component: AboutComponent },
-  // {
-  //   path: '',
-  //   redirectTo: 'newRelease',
-  //   pathMatch: 'full',
-  // },
+  { path: 'newRelease', component: NewReleasesComponent },
+  { path: 'artist', component: ArtistDiscographyComponent },
+  { path: 'album', component: AlbumComponent },
+  { path: 'about', component: AboutComponent },
+  {
+    path: '',
+    redirectTo: 'newRelease',
+    pathMatch: 'full',
+  },
+  { path: '404', component: NotFoundComponent },
 ];
 
 @NgModule({
