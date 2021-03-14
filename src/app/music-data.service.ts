e66b0b730a5069858adf1dc8e528d3f6dc0c1cb5
tuanthanh2067 <tuanthanh2067@gmail.com>
2021-03-14 18:38:25 -0400

    adding more func to music data service

--- src/app/music-data.service.ts
+++ src/app/music-data.service.ts
@@ -9,6 +9,8 @@ import { mergeMap } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class MusicDataService {
+  favouritesList: Array<any> = [];
+
   constructor(
     private spotifyToken: SpotifyTokenService,
     private http: HttpClient
@@ -23,5 +25,77 @@ export class MusicDataService {
         );
       })
     );
+  }
+
+  getArtistById(id): Observable<any> {
+    return this.spotifyToken.getBearerToken().pipe(
+      mergeMap((token) => {
+        return this.http.get<any>(`https://api.spotify.com/v1/artists/${id}`, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+      })
+    );
+  }
+
+  getAlbumsByArtistId(id): Observable<any> {
+    return this.spotifyToken.getBearerToken().pipe(
+      mergeMap((token) => {
+        return this.http.get<any>(
+          `https://api.spotify.com/v1/artists/${id}/albums?include_group=album,single&limit=50`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+      })
+    );
+  }
+
+  getAlbumById(id): Observable<any> {
+    return this.spotifyToken.getBearerToken().pipe(
+      mergeMap((token) => {
+        return this.http.get<any>(`https://api.spotify.com/v1/albums/${id}`, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+      })
+    );
+  }
+
+  searchArtists(searchString): Observable<any> {
+    return this.spotifyToken.getBearerToken().pipe(
+      mergeMap((token) => {
+        return this.http.get<any>(
+          `https://api.spotify.com/v1/search?q=${searchString}&type=artist&limit=50`,
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+      })
+    );
+  }
+
+  addToFavourites(id): boolean {
+    if (id === null || id === undefined || this.favouritesList.length >= 50) {
+      return false;
+    }
+    this.favouritesList.push(id);
+    return true;
+  }
+
+  removeFromFavourites(id): Observable<any> {
+    let index = this.favouritesList.indexOf(id);
+    this.favouritesList.splice(index);
+    return this.getFavourites();
+  }
+
+  getFavourites(): Observable<any> {
+    if (this.favouritesList.length > 0) {
+      return this.http.get<any>(
+        `https://api.spotify.com/v1/tracks?ids=${this.favouritesList.join()}`
+      );
+    } else {
+      return new Observable((o) => {
+        o.next([]); // return an empty array immediately to any subscribers
+      });
+    }
   }
 }
